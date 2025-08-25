ecbfc62e687063a86f6d33f1d8119ca57688e0cd
Kasidit-Leelasawad <kasidit1844@gmail.com>
2025-08-26 00:00:37 +0100

    Create Plots and assessment.py

---
+++ Test on mathematical functions/Plots and assessment.py
@@ -0,0 +1,2 @@
+#define integer ratio (r) and percentage of integer ratio. For example, integer ratio 0.2 = r20
+plot_performance(test_results, algorithms, functions, ".", dimensions, start_indices, ratio_key="r20")
